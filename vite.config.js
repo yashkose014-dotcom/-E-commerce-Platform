cd64a326cce28307034b8b95f04af488ae244a6c
Yash Kose <yashkose014@gmail.com>
2026-09-04 03:15:14 +0530

    fix github pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   base: '/E-commerce-Platform/',
+   base: '/-E-commerce-Platform/',
 })
